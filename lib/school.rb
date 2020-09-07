2082b9db13a54536c0ce2e93e71604e8690e7182
Justice Payne <spikepayne@yahoo.com>
2020-09-07 17:52:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,4 +8,12 @@ class School
     @roster
   end
   
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  
   
